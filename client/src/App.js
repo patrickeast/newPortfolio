22e293d6a373a1b53ef140f3ebf0284d1918806a
patrickeast <patrick.east@icloud.com>
2018-07-16 09:43:43 -0600

    Parallax features on new site.

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <div className="parallax"></div>
+        <div className="firstContainer">
+          <div className="container">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        </div>
+        <div className="secondContainer">
+          <p className="App-intro">
+            And here is some sample code to go along with the second container. Hopefully it fills the height.
+          </p>
+        </div>
       </div>
     );
   }
