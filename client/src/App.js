6ad66da8a7bedcc3f138d117654ba8073bd5a53f
patrickeast <patrick.east@icloud.com>
2018-07-17 15:51:28 -0600

    Changed parallax features.

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,17 @@ class App extends Component {
     return (
       <div className="App">
         <div className="parallax"></div>
-        <div className="firstContainer">
-          <div className="container">
+        <div className="firstContainer text-white">
+          <div className="text-white">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
+            <h2>To get started, edit App.JS and save to reload.</h2>
           </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         </div>
         <div className="secondContainer">
-          <p className="App-intro">
+          <h2 className="App-intro text-white">
             And here is some sample code to go along with the second container. Hopefully it fills the height.
-          </p>
+          </h2>
         </div>
       </div>
     );
