9959eeb8912118b7e06b0d53f725cb2d5a25c359
patrickeast <patrick.east@icloud.com>
2018-07-23 16:56:27 -0600

    New homepage, new changes.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar.js";
 import Footer from "./components/Footer/Footer.js";
+import Figment from "./images/figment.png";
+import BlankCanvas from "./images/blankCanvas.png";
+import Arnold from "./images/arnold.png";
 
 // ;(function (){
 //   var previousScroll = 0;
@@ -33,23 +36,129 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <div className="parallax">
+        <div className="parallax d-flex align-items-center">
           <div className="container-fluid">
-            <div className="row justify-content-between">
-              <div className="col-xs-10 col-sm-5 col-md-3">
+            <div className="row justify-content-around">
+              <div className="col-8">
+                <div className="jumbotron bg-dark jumbotron-fluid rounded">
+                  <div className="container">
+                    <div className="col-xs-10 col-md-12">
+                      <h1 className="text-white">Frontend Development, specializing in UI/UX Design</h1>
+                      <div className="iconContainer container">
+                        <div className="row">
+                          <div className="col-sm-12">
+                            <i className="text-white mr-4 pt-2 fab fa-react fa-3x"></i>
+                            <i className="text-white mr-4 pt-2 fab fa-html5 fa-3x"></i>
+                            <i className="text-white mr-4 pt-2 fab fa-css3-alt fa-3x"></i>
+                            <i className="text-white mr-4 pt-2 fab fa-js-square fa-3x"></i>
+                            <i className="text-white mr-4 pt-2 fab fa-node-js fa-3x"></i>
+                            <i className="text-white mr-4 pt-2 fab fa-github-alt fa-3x"></i>
+                            <i className="text-white mr-4 pt-2 fab fa-slack fa-3x"></i>
+                            <i className="text-white mr-4 pt-2 fab fa-wordpress fa-3x"></i>
+                            <i className="text-white mr-4 pt-2 fab fa-windows fa-3x"></i>
+                            <i className="text-white mr-4 pt-2 icon-mongodb"></i>
+
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="col-xs-10 col-sm-5 col-md-3">
-              </div>
-              <div className="col-xs-10 col-sm-5 col-md-3">
-              </div>
+
             </div>
           </div>
         </div>
-        <div className="firstContainer text-white">
-          <div className="text-white">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            <h2>To get started, edit App.JS and save to reload.</h2>
+        <div className="firstContainer">
+          <div classname="container-fluid">
+            <div className="row">
+              <div className="col-12">
+                <h1 className="text-center text-white">Just a few work examples...</h1>
+              </div>
+            </div>
+            <div className="row justify-content-between">
+              <div className="col-xs-10 col-sm-5 col-md-4">
+                <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                  <div class="mainflip">
+                    <div class="frontside">
+                      <div class="card">
+                        <img class="card-img-top img- fluid" src={Figment} alt="card image" />
+                      </div>
+                    </div>
+                    <div class="backside">
+                      <div class="card">
+                        <div class="card-header">
+                          This is a Header
+                        </div>
+                        <div class="card-body">
+                          <h4 class="card-title">Card Title</h4>
+                          <p class="card-text">This is a card component with header and footer.</p>
+                          <a href="#" class="btn btn-info btn-md">Info Button</a>
+                        </div>
+                        <div class="card-footer">
+                          This is a Footer
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-xs-10 col-sm-5 col-md-4">
+                <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                  <div class="mainflip">
+                    <div class="frontside">
+                      <div class="card">
+                        <img class="card-img-top img- fluid" src={Figment} alt="card image" />
+                      </div>
+                    </div>
+                    <div class="backside">
+                      <div class="card">
+                        <div class="card-header">
+                          This is a Header
+                        </div>
+                        <div class="card-body">
+                          <h4 class="card-title">Card Title</h4>
+                          <p class="card-text">This is a card component with header and footer.</p>
+                          <a href="#" class="btn btn-info btn-md">Info Button</a>
+                        </div>
+                        <div class="card-footer">
+                          This is a Footer
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-xs-10 col-sm-5 col-md-4">
+                <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                  <div class="mainflip">
+                    <div class="frontside">
+                      <div class="card">
+                        <img class="card-img-top img- fluid" src={Figment} alt="card image" />
+                      </div>
+                    </div>
+                    <div class="backside">
+                      <div class="card">
+                        <div class="card-header">
+                          This is a Header
+                        </div>
+                        <div class="card-body">
+                          <h4 class="card-title">Card Title</h4>
+                          <p class="card-text">This is a card component with header and footer.</p>
+                          <a href="#" class="btn btn-info btn-md">Info Button</a>
+                        </div>
+                        <div class="card-footer">
+                          This is a Footer
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
         <div className="secondContainer">
@@ -57,7 +166,7 @@ class App extends Component {
             And here is some sample code to go along with the second container. Hopefully it fills the height.
           </h2>
         </div>
-        <Footer /> 
+        <Footer />
       </div >
     );
   }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,40 +1,42 @@
 import React from "react";
 import "./Navbar.css";
 import $ from 'jquery';
+import East from "../../images/EastTop.png";
 // import logo from '../../images/East-Designs_Black.png';
 
 
 /**
  * Listen to scroll to change header opacity class
  */
-function checkScroll(){
+function checkScroll() {
   var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
-  if($(window).scrollTop() > startY){
-      $('.navbar').addClass("scrolled");
-  } else{
-      $('.navbar').removeClass("scrolled");
+  if ($(window).scrollTop() > startY) {
+    $('.navbar').addClass("scrolled");
+  } else {
+    $('.navbar').removeClass("scrolled");
   }
 }
 
-if($('.navbar').length > 0){
-  $(window).on("scroll load resize", function(){
-      checkScroll();
+if ($('.navbar').length > 0) {
+  $(window).on("scroll load resize", function () {
+    checkScroll();
   });
 }
 
 const Nav = props => (
-    <nav className="navbar navbar-expand-md fixed-top">
-      <div className="navbar-brand fixed">
-        <h5 className="App-logo" alt="logo">&lt;/p&gt;</h5>
-      </div>
-      <span className="text-dark mt-2"><h3>Patrick East</h3></span>
-      <div className="ml-auto mt-2 mt-md-0">
-        <a href="https://github.com/patrickeast" target="_blank"><i className="fab fa-github"></i></a>
-        <a href="https://www.linkedin.com/in/patrickeast/" target="_blank"><i className="fab fa-linkedin-in"></i></a>
-        <a href="mailto:patrick.east@icloud.com" target="_blank"><i className="fas fa-envelope"></i></a>
-      </div>
-    </nav>
+  <nav className="navbar navbar-expand-md fixed-top">
+    <div className="navbar-brand fixed">
+      <h5 className="App-logo" title="Just for fun" alt="logo">&lt;/p&gt;</h5>
+    </div>
+    {/* <span className="text-dark mt-2"><h3>Patrick East</h3></span> */}
+    <img src={East} alt="Patrick East" className="mx-auto mt-2 EastLogo" /><br />
+    <div className="ml-auto mt-2 mt-md-0 icons">
+      <a href="https://github.com/patrickeast" target="_blank" rel="noopener noreferrer" value="View my GitHub" title="View my GitHub"><i className="fab fa-github fa-1x"></i></a>
+      <a href="https://www.linkedin.com/in/patrickeast/" target="_blank" rel="noopener noreferrer" title="Follow my LinkedIn"><i className="fab fa-linkedin-in fa-1x"></i></a>
+      <a href="mailto:patrick.east@icloud.com" target="_blank" rel="noopener noreferrer" title="Send me a message"><i className="fas fa-envelope fa-1x"></i></a>
+    </div>
+  </nav>
 );
 
 export default Nav;
