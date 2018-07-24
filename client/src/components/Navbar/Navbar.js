bc73047e750731e47b1153d24daab6570f77ea77
patrickeast <patrick.east@icloud.com>
2018-07-24 13:53:31 -0600

    Changes to layout for mobile.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,29 +4,30 @@ import East from "../../images/EastTop.png";
 
 
 const Nav = props => (
-  <nav className="navbar navbar-expand-xs fixed-top">
-  {/* <nav className="navbar navbar-expand-md fixed-top d-flex bg-dark text-white"> */}
-    {/* <div className="tagline">Always learning, always coding.</div> */}
-    {/* <!-- Toggler/collapsibe Button --> */}
-      <ul>
-        <li className="text-left nav-item">
-          <div className="navbar-brand">
-            <h5 className="App-logo" title="Just for fun" alt="logo">&lt;/p&gt;</h5>
-          </div>
-        </li>
-        <li classname="nav-item">
-          <img src={East} alt="Patrick East" className="img-fluid EastLogo" />
-          {/* <h1>Patrick East</h1> */}
-        </li>
-        <li className="text-right nav-item">
-        <a href="https://github.com/patrickeast" target="_blank" rel="noopener noreferrer" value="View my GitHub" title="View my GitHub"><i className="fab fa-github fa-1x"></i></a>
-        <a href="https://www.linkedin.com/in/patrickeast/" target="_blank" rel="noopener noreferrer" title="Connect with me on LinkedIn"><i className="fab fa-linkedin-in fa-1x"></i></a>
-        <a href="mailto:patrick.east@icloud.com" target="_blank" rel="noopener noreferrer" title="Send me a message"><i className="fas fa-envelope fa-1x"></i></a>
+  <nav className="navbar navbar-expand-xs fixed-top d-flex">
+    {/* <nav className="navbar navbar-expand-md fixed-top d-flex bg-dark text-white"> */}
+    <ul className="d-flex align-items-start">
+      <li className="text-left nav-item">
+        <div className="navbar-brand">
+          <h5 className="App-logo" title="Just for fun" alt="logo">&lt;/p&gt;</h5>
+        </div>
       </li>
-      </ul>
+      <li classname="nav-item">
+        <img src={East} alt="Patrick East" className="img-fluid EastLogo" />
+        {/* <h1>Patrick East</h1> */}
+      </li>
+      <li className="text-right nav-item">
+        <div className="topIcons">
+          <a href="https://github.com/patrickeast" target="_blank" rel="noopener noreferrer" value="View my GitHub" title="View my GitHub"><i className="fab fa-github fa-1x"></i></a>
+          <a href="https://www.linkedin.com/in/patrickeast/" target="_blank" rel="noopener noreferrer" title="Connect with me on LinkedIn"><i className="fab fa-linkedin-in fa-1x"></i></a>
+          <a href="mailto:patrick.east@icloud.com" target="_blank" rel="noopener noreferrer" title="Send me a message"><i className="fas fa-envelope fa-1x"></i></a>
+        </div>
+      </li>
+    </ul>
+    {/* <div className="tagline">Always learning, always coding.</div> */}
     {/* <ul className="d-flex align-items-start"> */}
-      
-     
+
+
   </nav>
 );
 
